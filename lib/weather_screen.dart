81d1024ec43d55057e31d946f9c5367fd4d87ba6
eduardolluis <1132907@est.intec.edu.do>
2026-01-04 12:44:29 -0400

    layout app added

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -21,6 +21,22 @@ class WeatherScreen extends StatelessWidget {
           ),
         ],
       ),
+      body: Column(
+        children: [
+          // main card
+          const Placeholder(fallbackHeight: 150),
+
+          const SizedBox(height: 20),
+
+          // weather forecast
+          const Placeholder(fallbackHeight: 150),
+
+          const SizedBox(height: 20),
+
+          // additional weather details
+          const Placeholder(fallbackHeight: 150),
+        ],
+      ),
     );
   }
 }
