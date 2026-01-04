4680293dbbea3026066ceabaa25c2179ac245c47
eduardolluis <1132907@est.intec.edu.do>
2026-01-04 12:38:05 -0400

    icon button widget

---
+++ lib/weather_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class WeatherScreen extends StatelessWidget {
+  const WeatherScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Weather App",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              print("Refresh weather data");
+            },
+            icon: Icon(Icons.refresh),
+          ),
+        ],
+      ),
+    );
+  }
+}
