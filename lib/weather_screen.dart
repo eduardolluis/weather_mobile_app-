a9dcfb071460e097ee477e9bc5b0add6780acfc8
eduardolluis <1132907@est.intec.edu.do>
2026-01-05 09:20:32 -0400

    backdrop and imageFilter widget

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class WeatherScreen extends StatelessWidget {
@@ -24,33 +26,64 @@ class WeatherScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             // main card
             SizedBox(
               width: double.infinity,
               child: Card(
-                elevation: 10,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(16),
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        "300° F",
-                        style: TextStyle(
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold,
-                        ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            "300° F",
+                            style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          Icon(Icons.cloud, size: 64),
+                          Text("Cloudy", style: TextStyle(fontSize: 20)),
+                        ],
                       ),
-                      const SizedBox(height: 16),
-                      Icon(Icons.cloud, size: 64),
-                      Text("Cloudy", style: TextStyle(fontSize: 20)),
-                    ],
+                    ),
                   ),
                 ),
               ),
+            ),
+            const SizedBox(height: 20),
+
+            const Text(
+              "Weather Forecast",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+
+            const SizedBox(height: 20
+            ),
+
+            Row(
+              children: [
+                SizedBox(
+                  width: 100,
+                  child: Card(
+                    child: Column(
+                      children: [
+                        Text("03:00"),
+                        
+                      ],
+                    ),
+                  ),
+                )
+              ],
             ),
             const Placeholder(fallbackHeight: 150),
 
@@ -62,7 +95,6 @@ class WeatherScreen extends StatelessWidget {
             const SizedBox(height: 20),
 
             // additional weather details
-            const Placeholder(fallbackHeight: 150),
           ],
         ),
       ),
